361be7e975d87944de665d3eeddbaec2db3c6886
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2022-05-04 04:33:48 +0600

    Add files via upload

---
+++ 110A Nearly Lucky Number.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n; // Here LONG LONG INT is a MUST !!!!
+    cin>>n;
+    int i,total=0;
+
+    while(n)
+    {
+        if(n%10==4 || n%10==7){total++;}
+        n=n/10;
+    }
+
+    if(total ==4 || total==7){cout<<"YES"<<endl;}
+    else {cout<<"NO"<<endl;}
+
+
+
+
+    return 0;
+}
